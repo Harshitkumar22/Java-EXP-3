b52b0bcc00235cb8e4361d55e36103529fda92c8
Harshitkumar22 <harshitkumar1463@gmail.com>
2025-02-04 09:23:11 +0530

    Update MoveZeros.java

--- MoveZeros.java
+++ MoveZeros.java
@@ -1,3 +1,9 @@
+//3) Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+//Example:
+//Input: nums = [0,1,0,3,12]
+//Output: [1,3,12,0,0]
+
+
 public class MoveZeros{
     public static void moveZeroes(int[] nums){
         int count=0;
